eeada7f2bb452ca73d77bc09806a29ed7a28e5b1
Rigó Máté <mate@rigo.co.hu>
2023-08-27 22:46:42 +0200

    embed python which is natively build for windows

--- ci-build.sh
+++ ci-build.sh
@@ -6,11 +6,8 @@ wget https://ftp.gnu.org/gnu/gdb/gdb-${gdbVersion}.tar.xz
 tar -xf gdb-${gdbVersion}.tar.xz
 
 pushd gdb-${gdbVersion}
-
-wget https://www.python.org/ftp/python/3.11.5/python-3.11.5-embed-amd64.zip
-unzip python-3.11.5-embed-amd64.zip -d python3.11
 echo "[i] configuring"
-./configure --target=nios2-elf --with-python=$(pwd)/python3.11 --without-auto-load-safe-path --with-gdb-datadir=c:/intelfpga_lite/22.1std/nios2eds/bin/gnu/h-x86_64-mingw32/nios2-elf/share/gdb
+./configure --target=nios2-elf --with-python --without-auto-load-safe-path --with-gdb-datadir=c:/intelfpga_lite/22.1std/nios2eds/bin/gnu/h-x86_64-mingw32/nios2-elf/share/gdb
 echo "[i] starting build"
 make all-gdb -j$(nproc)
 strip gdb/gdb.exe
@@ -25,6 +22,11 @@ echo "[i] copying gdb.exe and .dll files"
 mkdir -p assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin
 cp ./gdb-${gdbVersion}/gdb/gdb.exe ./assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin
 cp /mingw64/bin/*.dll ./assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin
+
+echo "[i] copying get python dll from embeddeble source"
+wget https://www.python.org/ftp/python/3.11.5/python-3.11.5-embed-amd64.zip
+unzip python-3.11.5-embed-amd64.zip -d python3.11
+cp -f ./python3.11/python311.dll ./assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin/libpython3.11.dll
 
 pushd assets
 echo "[i] tarballing final artifact"
