82fc8128f3ad6b4bef2d5f7d8ed268878936d372
Rigó Máté <mate@rigo.co.hu>
2023-08-27 23:20:43 +0200

    use relative path

--- ci-build.sh
+++ ci-build.sh
@@ -7,7 +7,7 @@ tar -xf gdb-${gdbVersion}.tar.xz
 
 pushd gdb-${gdbVersion}
 echo "[i] configuring"
-./configure --target=nios2-elf --with-python --without-auto-load-safe-path --with-gdb-datadir=c:/intelfpga_lite/22.1std/nios2eds/bin/gnu/h-x86_64-mingw32/nios2-elf/share/gdb
+./configure --target=nios2-elf --with-python --without-auto-load-safe-path --with-gdb-datadir=../nios2-elf/share/gdb
 echo "[i] starting build"
 make all-gdb -j$(nproc)
 strip gdb/gdb.exe
@@ -20,7 +20,7 @@ cp -r ./gdb-${gdbVersion}/gdb/data-directory/python ./assets/intelFPGA_lite/22.1
 
 echo "[i] copying gdb.exe and .dll files"
 mkdir -p assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin
-cp ./gdb-${gdbVersion}/gdb/gdb.exe ./assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin
+cp ./gdb-${gdbVersion}/gdb/gdb.exe ./assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin/nios2-elf-gdb.exe
 cp /mingw64/bin/*.dll ./assets/intelFPGA_lite/22.1std/nios2eds/bin/gnu/H-x86_64-mingw32/bin
 
 echo "[i] copying get python dll from embeddeble source"
